9e6a022205b3c52f61eff407359d457dbeb1778d
RKDNN <mbridges.code@gmail.com>
2025-11-10 11:45:36 +0000

    Recursively decrypt serialized mongoose docs

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,6 +1,46 @@
+const { decryptValue, ENC_PREFIX } = require('./encryption');
+
 const defaultToObjectOptions = {
   getters: true,
   virtuals: true,
+};
+
+const isPlainObject = (value) => (
+  value !== null
+  && typeof value === 'object'
+  && Object.prototype.toString.call(value) === '[object Object]'
+);
+
+const safeDecrypt = (value) => {
+  if (typeof value !== 'string' || !value.startsWith(ENC_PREFIX)) {
+    return value;
+  }
+
+  try {
+    return decryptValue(value);
+  } catch (error) {
+    console.error('Failed to decrypt value during serialization', { message: error?.message });
+    return value;
+  }
+};
+
+const deepNormalize = (input) => {
+  if (Array.isArray(input)) {
+    return input.map((item) => deepNormalize(item));
+  }
+
+  if (input instanceof Date || input instanceof RegExp || Buffer.isBuffer(input)) {
+    return input;
+  }
+
+  if (isPlainObject(input)) {
+    return Object.entries(input).reduce((acc, [key, value]) => {
+      acc[key] = deepNormalize(value);
+      return acc;
+    }, {});
+  }
+
+  return safeDecrypt(input);
 };
 
 const toPlainObject = (input, options = defaultToObjectOptions) => {
@@ -13,10 +53,10 @@ const toPlainObject = (input, options = defaultToObjectOptions) => {
   }
 
   if (typeof input.toObject === 'function') {
-    return input.toObject(options);
+    return deepNormalize(input.toObject(options));
   }
 
-  return input;
+  return deepNormalize(input);
 };
 
 module.exports = {
