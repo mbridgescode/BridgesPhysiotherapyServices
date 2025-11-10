8795183117457b9dca8bd44f302ffabc2a24cc06
RKDNN <mbridges.code@gmail.com>
2025-11-10 00:44:54 +0000

    Ensure serverless handler strips /api prefix before delegating

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const app = require('../server/app');
 const { connectToDatabase } = require('../server/database');
 
+const stripApiPrefix = (url = '') => url.replace(/^\/api(\/|$)/, '/');
+
 module.exports = async (req, res) => {
   try {
     await connectToDatabase();
+    req.url = stripApiPrefix(req.url) || '/';
     return app(req, res);
   } catch (error) {
     console.error('API handler error', error);
